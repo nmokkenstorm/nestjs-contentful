8206823ede190eb41fd130956c538b1f44376b5b
Niels Mokkenstorm <niels@mokkenstorm.dev>
2023-08-16 14:03:42 +0200

    feat: add file source

---
+++ lib/schema-source-file.service.ts
@@ -0,0 +1,75 @@
+import { join } from 'path'
+import { existsSync, mkdirSync } from 'fs'
+import { readFile, writeFile } from 'fs/promises'
+
+import { SchemaSource, Schema, SchemaOptions } from './schema-source.interface'
+
+const defaultOptions: FileSchemaSourceOptions = {
+  space: 'default',
+  environment: 'master',
+  baseDir: join(__dirname, '../', 'schemas'),
+}
+
+type FileSchemaSourceOptions = SchemaOptions & {
+  baseDir: string
+  file?: string
+}
+
+export class FileSchemaSource implements SchemaSource {
+  private readonly options: FileSchemaSourceOptions
+
+  constructor(options: Partial<FileSchemaSourceOptions> = {}) {
+    this.options = { ...defaultOptions, ...options }
+  }
+
+  read = async (
+    options: Partial<FileSchemaSourceOptions> = {},
+  ): Promise<Schema> => {
+    const parsedOptions = this.parseOptions(options)
+
+    const path = await this.getFilePath(parsedOptions)
+
+    return JSON.parse((await readFile(path)).toString())
+  }
+
+  write = async (
+    schema: Schema,
+    options: Partial<FileSchemaSourceOptions> = {},
+  ): Promise<void> => {
+    const parsedOptions = this.parseOptions(options)
+
+    const path = await this.getFilePath(parsedOptions)
+
+    await writeFile(path, JSON.stringify(schema))
+  }
+
+  private parseOptions = (
+    options: Partial<FileSchemaSourceOptions> = {},
+  ): FileSchemaSourceOptions => ({ ...this.options, ...options })
+
+  private getDir = async ({
+    baseDir,
+    space,
+  }: FileSchemaSourceOptions): Promise<string> => {
+    const path = join(baseDir, space)
+
+    this.ensureExists(path)
+
+    return path
+  }
+
+  private ensureExists(path: string): void {
+    if (!existsSync(path)) {
+      mkdirSync(path, { recursive: true })
+    }
+  }
+
+  private getFilePath = async (
+    options: FileSchemaSourceOptions,
+  ): Promise<string> => {
+    const { file, environment } = options
+    const path = join(await this.getDir(options), file ?? environment)
+
+    return path.endsWith(".json") ? path : `${path}.json`
+  }
+}
---
+++ tests/e2e/file-schema-source.spec.ts
@@ -0,0 +1,73 @@
+import { FileSchemaSource } from "../../lib/schema-source-file.service"
+import { Schema } from "../../lib/schema-source.interface"
+import { rmdir, writeFile, mkdir } from "fs/promises"
+import { join } from "path"
+
+describe("File Schema Source", () => {
+  const baseDir = join(__dirname, "../", "fixtures", "schemas")
+
+  let source: FileSchemaSource
+  const schema: Schema = {
+    contentTypes: [],
+  }
+
+  const createFile = async (directory: string, file: string) => {
+    const path = join(baseDir, directory, file)
+
+    await mkdir(join(baseDir, directory), { recursive: true })
+
+    await writeFile(path, JSON.stringify(schema))
+  }
+
+  beforeAll(async () => {
+    try {
+      await rmdir(baseDir, { recursive: true })
+      await mkdir(baseDir, { recursive: true })
+    } catch (e) {
+      // already empty
+    }
+  })
+
+  beforeEach(() => {
+    source = new FileSchemaSource({
+      baseDir,
+    })
+  })
+
+  it("should be able to get a schema from file", async () => {
+    await createFile("default", "master.json")
+
+    const result = await source.read()
+    expect(result).toBeDefined()
+  })
+
+  it("should be able to get a schema from a custom file with an extension", async () => {
+    await createFile("default", "master-custom.json")
+    const result = await source.read({ file: "master-custom" })
+    expect(result).toBeDefined()
+  })
+
+  it("should be able to get a schema from a custom file without an extension", async () => {
+    await createFile("default", "master-custom-2.json")
+
+    const result = await source.read({ file: "master-custom-2.json" })
+    expect(result).toBeDefined()
+  })
+
+  it("should be able to write a schema to a directory", async () => {
+    await source.write(schema)
+    expect(schema).toBeDefined()
+  })
+
+  it("should be able to write a schema to a custom directory", async () => {
+    await source.write(schema, { baseDir: join(__dirname, "../", "schemas") })
+    expect(schema).toBeDefined()
+  })
+
+  it("should be able to write a schema to a custom file", async () => {
+    const file = "master-test.json"
+
+    await source.write(schema, { file })
+    expect(schema).toBeDefined()
+  })
+})
